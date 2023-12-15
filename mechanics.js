6f7b6992a68cc4e2eb7bda8644159b0ace8b55dc
Fred <leferpz@gmail.com>
2023-12-15 17:14:42 -0500

    Changed it to be function-based

--- mechanics.js
+++ mechanics.js
@@ -1,43 +1,44 @@
 let choices = ["Rock", "Paper", "Scissors"];
-let computerChoice = choices[(Math.floor(Math.random() * choices.length))];
-let playerSelection = prompt("Rock, paper, or scissors?");
 
-console.log(computerChoice);
-console.log(playerSelection);
+function getComputerChoice() {
+    let computerSelection = choices[(Math.floor(Math.random() * choices.length))]
+    return computerSelection;
+}
 
-function playRound(playerSelection, computerChoice) {
-    let string = playerSelection.toString();
-    let lowerCaseSelection = string.toLowerCase();
-    if (lowerCaseSelection === "rock" && computerChoice === "Paper") {
+function getPlayerChoice () {
+    let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+    return playerSelection;
+}
+
+function playRound() {
+    let playerSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+    if (playerSelection === "rock" && computerSelection === "Paper") {
         console.log("You lose! Computer picked paper")
     }
-    else if (lowerCaseSelection === "rock" && computerChoice === "Scissors") {
+    else if (playerSelection === "rock" && computerSelection === "Scissors") {
         console.log("You win! Computer picked scissors")
     }
-    else if (lowerCaseSelection === "rock" && computerChoice === "Rock") {
-        console.log("Tie! Computer picked rock")
-    }
-    else if (lowerCaseSelection === "paper" && computerChoice === "Paper") {
-    console.log("Tie! Computer picked paper")
-    }
-    else if (lowerCaseSelection === "paper" && computerChoice === "Scissors") {
+    else if (playerSelection === "paper" && computerSelection === "Scissors") {
         console.log("You lose! Computer picked scissors")
     }
-    else if (lowerCaseSelection === "paper" && computerChoice === "Rock") {
+    else if (playerSelection === "paper" && computerSelection === "Rock") {
         console.log("You win! Computer picked rock")
     }
-    else if (lowerCaseSelection === "scissors" && computerChoice === "Rock") {
+    else if (playerSelection === "scissors" && computerSelection === "Rock") {
         console.log("You lose! Computer picked rock")
     }
-    else if (lowerCaseSelection === "scissors" && computerChoice === "Scissors") {
-        console.log("Tie! Computer picked rock")
+    else if (playerSelection === "scissors" && computerSelection === "Paper") {
+            console.log("You win! Computer picked paper")
     }
-    else if (lowerCaseSelection === "scissors" && computerChoice === "Paper") {
-        console.log("You win! Computer picked paper")
+    else if (playerSelection === computerSelection) {
+        console.log("Tie! Play again?")
+        playRound();
     }
     else {
         console.log("Sorry, I don't know what you said. Try 'rock', 'paper', or 'scissors' without punctuation!")
+        playRound();
     }
 }
 
-playRound(playerSelection, computerChoice);
+playRound();
